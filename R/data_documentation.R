de9320cb27b41c8d0f7c43817f011ca1d9425194
MTurner0 <margaret.turner1416@gmail.com>
2022-04-27 08:49:47 -0500

    Created 'toy' dataset: .

---
+++ R/data_documentation.R
@@ -0,0 +1,14 @@
+#' @title A `MultiAssayExperiment` Object
+#' 
+#' @description 26 patients were randomly assigned to a `treatment` or `placebo`.
+#' 
+#' On each of 5 visits, stool samples were obtained for 16S rRNA-seq (of 10 bacterial species).
+#' However, 5 of the sequencing results failed quality control and had to be discarded.
+#' The results are stored in the `counts` assay in the `phy16S` `SummarizedExperiment`.
+#' 
+#' On visits 1, 3, and 5, the concentrations of 8 cytokines were measured.
+#' These results are stored in the `cyto_conc` assay in the `cyto` `SummarizedExperiment`.
+#' 
+#' @examples 
+#' print(mae)
+"mae"
