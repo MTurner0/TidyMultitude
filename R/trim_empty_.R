07a54f9b481a604e2934fceccc1417053031bea1
MTurner0 <margaret.turner1416@gmail.com>
2022-10-18 10:00:10 -0500

    Refactored intersect_colData and added tests. Linting.

--- R/trim_empty_.R
+++ R/trim_empty_.R
@@ -2,12 +2,12 @@
 #'
 #' @description Sequencing data often produces large but sparse matrices. To
 #'   save space and time, it may be worthwhile to remove empty columns and/or
-#'   rows. Note that a row will only be removed if it is empty for all assays
-#'   in a Summarized Experiment.
+#'   rows. Note that a row will only be removed if it is empty for all assays in
+#'   a Summarized Experiment.
 #'
 #' @param .data Either a `MultiAssayExperiment` or a `SummarizedExperiment`.
-#' @param counts If `TRUE`, empty rows can be checked using `rowSums(x) > 0`, which
-#'   is faster than `any(x != 0)`.
+#' @param counts If `TRUE`, empty rows can be checked using `rowSums(x) > 0`,
+#'   which is faster than `any(x != 0)`.
 #'
 #' @export
 trim_empty_rows <- function(.data, experiment, counts = TRUE) {
@@ -16,22 +16,25 @@ trim_empty_rows <- function(.data, experiment, counts = TRUE) {
 
 #' @rdname trim_empty_rows
 #' @export
-trim_empty_rows.MultiAssayExperiment <- function(.data, experiment, counts = TRUE) {
+trim_empty_rows.MultiAssayExperiment <- function(
+    .data, experiment, counts = TRUE
+    ) {
   exp_name <- paste0(substitute(experiment))
-  .data[[exp_name]] <- trim_empty_rows.SummarizedExperiment(.data[[exp_name]],
-                                                            counts = counts)
+  .data[[exp_name]] <- trim_empty_rows(.data[[exp_name]], counts = counts)
   return(.data)
 }
 
 #' @rdname trim_empty_rows
+#' @importFrom SummarizedExperiment colData
+#' @importFrom SummarizedExperiment rowData
 #' @export
 trim_empty_rows.SummarizedExperiment <- function(.data, counts = TRUE) {
   # Create a vector identifying rows
   if (counts) {
-    nonempty_indices <- (sapply(as.list(assays(.data)), rowSums) > 0) %>% 
+    nonempty_indices <- (sapply(as.list(assays(.data)), rowSums) > 0) %>%
       apply(., 1, any)
   } else {
-    
+
     # Define internal functions
     matrix_nonzero <- function(x) {
       apply(x, 1, nonzero)
@@ -39,16 +42,24 @@ trim_empty_rows.SummarizedExperiment <- function(.data, counts = TRUE) {
     nonzero <- function(x) {
       any(x != 0)
     }
-    
-    nonempty_indices <- sapply(as.list(assays(.data)), matrix_nonzero) %>% 
+
+    nonempty_indices <- sapply(as.list(assays(.data)), matrix_nonzero) %>%
       apply(., 1, any)
   }
-  
+
   new_rowdata <- rowData(.data)[nonempty_indices, ]
-  new_assay_list <- purrr::map(.x = as.list(assays(.data)),
-                               ~ .x[nonempty_indices, ])
-  names(new_assay_list) <- assays(.data) %>% names()
-  SummarizedExperiment(assays = new_assay_list,
-                       colData = colData(.data),
-                       rowData = new_rowdata)
+
+  new_assay_list <- purrr::map(
+    .x = as.list(assays(.data)),
+    ~ .x[nonempty_indices, ]
+    )
+
+  names(new_assay_list) <- assays(.data) %>%
+    names()
+
+  SummarizedExperiment(
+    assays = new_assay_list,
+    colData = colData(.data),
+    rowData = new_rowdata
+    )
 }
