0c769ffb83bed1bf69d9306a9daf94d85240afe5
hbnarayanan <60229720+hbnarayanan@users.noreply.github.com>
2022-10-18 00:53:33 -0500

    Update test-mutate-transmute.R
    fixed some naming convention

--- tests/testthat/test-mutate-transmute.R
+++ tests/testthat/test-mutate-transmute.R
@@ -18,14 +18,14 @@ test_that("Adding a new Assay to Summarized Experiment", {
 
 test_that("Subsetting data on Summarized Experiment while dropping rest of summarized experiments", {
   data("mae")
-  mae1=transmute(mae,subset = cyto[1:5, 1:3])
-  expect_equal(dim(mae1[[1]]),dim(mae[[2]][1:5, 1:3]) )
-  expect_equal(mae1[['subset']], mae[[2]][1:5, 1:3])
-  expect_s4_class(mae1[[1]], "SummarizedExperiment")
+  mae3=transmute(mae,subset = cyto[1:5, 1:3])
+  expect_equal(dim(mae3[[1]]),dim(mae[[2]][1:5, 1:3]) )
+  expect_equal(mae3[['subset']], mae[[2]][1:5, 1:3])
+  expect_s4_class(mae3[[1]], "SummarizedExperiment")
 })
 
 test_that("Adding new assay and drops existing assays from Summarized Experiment", {
   data("mae")
-  mae3=mae %>% transmute(cyto, plus_one = cyto_conc + 1)
-  expect_equal(c(assayNames(mae3[[2]])), c("plus_one"))
+  mae4=mae %>% transmute(cyto, plus_one = cyto_conc + 1)
+  expect_equal(c(assayNames(mae4[[2]])), c("plus_one"))
 })
